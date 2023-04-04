664abaa75f7168e9976e3f9dcb484c7a04a99606
Jeff Dinkelman <mfankenstein@yahoo.com>
2023-04-03 22:10:02 -0700

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,28 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+
+const config: HardhatUserConfig = {
+  zksolc: {
+    version: "1.3.5",
+    compilerSource: "binary",
+    settings: {},
+  },
+  defaultNetwork: "zkSyncTestnet",
+  networks: {
+    hardhat: {
+      zksync: false,
+    },
+    zkSyncTestnet: {
+      url: "http://localhost:3050",
+      ethNetwork: "http://localhost:8545",
+      zksync: true,
+    },
+  },
+  solidity: {
+    version: "0.8.17",
+  },
+};
+
+export default config;
